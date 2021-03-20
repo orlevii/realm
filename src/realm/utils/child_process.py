3ad23dc29f75e51dba6ae19ea7ad66d5d20f081c
Or Levi <orlevi128@gmail.com>
2021-03-20 20:14:11 +0200

    Subprocess - support shell

--- src/realm/utils/child_process.py
+++ src/realm/utils/child_process.py
@@ -7,10 +7,13 @@ class ChildProcess:
 
     @staticmethod
     def run(command, **kwargs) -> str:
-        p = subprocess.Popen(shlex.split(command),
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE,
-                             **kwargs)
+        params = dict(stdout=subprocess.PIPE,
+                      stderr=subprocess.PIPE)
+        params.update(kwargs)
+        if not kwargs.get('shell'):
+            command = shlex.split(command)
+        p = subprocess.Popen(command,
+                             **params)
         out, err = p.communicate()
         if p.returncode != 0:
             raise RuntimeError(f'Failed running command: {command}, stdout: {out}, stderr: {err}')

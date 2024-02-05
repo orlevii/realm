2e6396c82d87c7d36bda510158f479724faa3a90
Or Levi <orlevi128@gmail.com>
2024-02-05 13:03:31 +0000

    example e2e test

---
+++ e2e/realm-e2e/tests/test_version.py
@@ -0,0 +1,12 @@
+import unittest
+
+from realm.utils.child_process import ChildProcess
+
+
+class TestVersion(unittest.TestCase):
+    def setUp(self):
+        ChildProcess.FORCE_CAPTURE = True
+
+    def test_version(self):
+        out = ChildProcess.run('realm -V')
+        self.assertIn('Realm', out)    

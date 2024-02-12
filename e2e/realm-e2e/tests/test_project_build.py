58e6d92686cff5b2b50087cff59f1b7d37e71000
Or Levi <orlevi128@gmail.com>
2024-02-12 20:21:11 +0000

    Pending changes exported from your codespace

---
+++ e2e/realm-e2e/tests/test_project_build.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import pytest
+from realm.utils.child_process import ChildProcess
+from tests.common import temp_git_branch
+from testcontainers
+
+@pytest.fixture(scope='module')
+def pypi_server():
+    

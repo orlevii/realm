75c9fb3a80ecad5e832aae0478c54ecea1533a39
Or Levi <orlevi128@gmail.com>
2024-02-18 22:31:27 +0000

    test publish with dependencies

--- e2e/realm-e2e/tests/test_project_build.py
+++ e2e/realm-e2e/tests/test_project_build.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 from pathlib import Path
 
@@ -40,5 +41,47 @@ def test_simple_publish(clean_repo, pypi_server):
     python_path = _create_venv(clean_repo)
     pip = f"{python_path} -m pip"
     ChildProcess.run(f"{pip} install pkg --index-url {pypi_server}", cwd=clean_repo)
-    res = ChildProcess.run(f"{pip} freeze", cwd=clean_repo)
+    res = ChildProcess.run(f"{pip} freeze", cwd=clean_repo).split()
     assert "pkg==0.1.0" in res
+
+
+INSTALL_SCENARIOS = [
+    {
+        "install": "dep-a",
+        "expected": {"dep-a==0.1.0", "dep-b==0.2.0", "dep-c==0.3.0"},
+    },
+    {
+        "install": "dep-b",
+        "expected": {"dep-b==0.2.0", "dep-c==0.3.0"},
+    },
+    {
+        "install": "dep-c",
+        "expected": {"dep-c==0.3.0"},
+    },
+]
+
+
+def test_publish_with_dependencies(clean_repo, pypi_server):
+    ChildProcess.run(
+        f'realm run --scope dep_* -- poetry config repositories.pypi-local "{pypi_server}" --local',
+        cwd=clean_repo,
+    )
+    ChildProcess.run("realm build --scope dep_*", cwd=clean_repo)
+    ChildProcess.run(
+        "realm run --scope dep_* -- poetry publish -r pypi-local -u admin -p admin",
+        cwd=clean_repo,
+    )
+    venv_path = Path(clean_repo).joinpath("venv")
+
+    for p in INSTALL_SCENARIOS:
+        install = p["install"]
+        expected = p["expected"]
+        python_path = _create_venv(clean_repo)
+        pip = f"{python_path} -m pip"
+        ChildProcess.run(
+            f"{pip} install {install} --index-url {pypi_server}", cwd=clean_repo
+        )
+        res = ChildProcess.run(f"{pip} freeze", cwd=clean_repo).split()
+        assert set(res) == expected, f"Install: {install}; Got: {res}"
+
+        shutil.rmtree(venv_path, ignore_errors=True)

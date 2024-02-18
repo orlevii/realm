0295398ba7dd7a6bfdc92cfea0fe6f78f337e13f
Or Levi <orlevi128@gmail.com>
2024-02-18 20:23:18 +0000

    test installation

--- e2e/realm-e2e/tests/test_project_build.py
+++ e2e/realm-e2e/tests/test_project_build.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+
 import pytest
 from realm.utils.child_process import ChildProcess
 from tests.test_services.pypiserver import PypiServer
@@ -7,6 +10,15 @@ from tests.test_services.pypiserver import PypiServer
 def pypi_server():
     with PypiServer() as pypi:
         yield pypi.get_url()
+
+
+def _create_venv(clean_repo) -> Path:
+    clean_repo_path = Path(clean_repo)
+    venv_path = clean_repo_path.joinpath("venv")
+    ChildProcess.run(
+        f"{sys.executable} -m venv venv", cwd=clean_repo
+    )
+    return venv_path
 
 
 def test_simple_publish(clean_repo, pypi_server):
@@ -19,3 +31,9 @@ def test_simple_publish(clean_repo, pypi_server):
         "realm run --scope pkg -- poetry publish -r pypi-local -u admin -p admin",
         cwd=clean_repo,
     )
+
+    venv_path = _create_venv(clean_repo)
+    pip = venv_path.joinpath("bin").joinpath("pip")
+    ChildProcess.run(f"{pip} install pkg --index-url {pypi_server}", cwd=clean_repo)
+    res = ChildProcess.run(f"{pip} freeze", cwd=clean_repo)
+    assert "pkg==0.1.0" in res
